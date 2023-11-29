63dd7335afd9db7bdcfde9b2d07429bde8e55811
psalmistcom <dadewale9@gmail.com>
2023-11-29 19:11:34 +0100

    fixes

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -6,7 +6,39 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(child: Text("Hello Flutter")),
+      backgroundColor: Colors.blueGrey,
+      body: Padding(
+        padding: EdgeInsets.all(35.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                '0',
+                style: TextStyle(
+                  color: Color.fromARGB(255, 200, 255, 204),
+                  // backgroundColor: Color.fromRGBO(20, 0, 0, 1),
+                  fontWeight: FontWeight.bold,
+                  fontSize: 45,
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(50.0),
+                      ),
+                    ),
+                    labelText: "Enter something here",
+                    hintText: 'Enter Text'),
+                style: TextStyle(
+                  color: Color.fromARGB(255, 255, 255, 255),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
